1322851028892ffbf10e43744365722597b44473
ch4mpy <ch4mp@c4-soft.com>
2024-01-26 07:19:39 -1000

    WiP

--- react-ui/app/page.tsx
+++ react-ui/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import Link from "next/link";
 import { useUserContext } from "./layout";
-
 
 export default function Home() {
   const user = useUserContext();
@@ -12,9 +12,17 @@ export default function Home() {
     : "You are not authenticated.";
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
+    <main className="min-h-screen">
+      <div className="flex">
+        <span className="ml-2"></span>
+        <button>
+          <Link href="/about">About</Link>
+        </button>
+        <span className="m-auto"></span>
         <h1>Home</h1>
+        <span className="m-auto"></span>
+      </div>
+      <div className="flex flex-col items-center justify-between p-24">
         <p>{message}</p>
       </div>
     </main>
